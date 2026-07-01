9ca062c2e32b10f841db3dc912f8f1ad7affeac2
Stefan Monov <logixoul@gmail.com>
2026-07-01 18:46:45 +0300

    code improvements and step1 sketch

--- sketches/self-driving-car-step1.js
+++ sketches/self-driving-car-step1.js
@@ -1,8 +1,5 @@
-let destPos, destDir;
+let startPos, destDir;
 let splineX, splineY;
-let interpCoef = 0.0;
-let carImage;
-let carPos;
 
 // изчислява стойността на сплайн функцията
 function calcSpline(s, t) {
@@ -21,46 +18,42 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
 
     imageMode(CENTER);
-    destPos = createVector(0, 0);
+    startPos = createVector(0, 0);
     destDir = createVector(0, 0);
-    carImage = loadImage(MEDIA_FOLDER + '/car.png');
 }
 
 function draw() {
     background(100);
     if (splineX) {
-        interpCoef = min(interpCoef + .01, 1.0);
-        const interpCoefSmooth = interpCoef * interpCoef * (3 - interpCoef * 2);
-        const newCarPos = createVector(
-            calcSpline(splineX, interpCoefSmooth),
-            calcSpline(splineY, interpCoefSmooth));
-        const movement = p5.Vector.sub(newCarPos, carPos);
-        if (movement.mag() > 0)
-            carAngle = atan2(movement.y, movement.x);
-        carPos = newCarPos;
-        translate(carPos.x, carPos.y);
-        rotate(carAngle);
-        image(carImage, 0, 0);
+        noFill();
+        beginShape();
+        for(let coef = 0; coef < 1; coef += .01) {
+            const pos = createVector(
+                calcSpline(splineX, coef),
+                calcSpline(splineY, coef));
+            vertex(pos.x, pos.y);
+        }
+        endShape();
     }
 }
 
 function mousePressed() {
     const newDestPos = createVector(mouseX, mouseY);
-    const offset = p5.Vector.sub(newDestPos, destPos);
+    const offset = p5.Vector.sub(newDestPos, startPos);
     const newDestDir = p5.Vector.mult(offset, 2.0);
     splineX = {
-        valueAt0: destPos.x,
+        valueAt0: startPos.x,
         valueAt1: newDestPos.x,
         derAt0: destDir.x,
         derAt1: newDestDir.x
     };
     splineY = {
-        valueAt0: destPos.y,
+        valueAt0: startPos.y,
         valueAt1: newDestPos.y,
         derAt0: destDir.y,
         derAt1: newDestDir.y
     };
-    destPos = newDestPos;
+    startPos = newDestPos;
     destDir = newDestDir;
     interpCoef = 0.0;
 }
